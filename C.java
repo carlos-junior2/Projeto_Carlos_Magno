4077190d1556330d90c152174c6f7cc408b95b56
unknown <juninhonasc3@gmail.com>
2022-02-03 20:39:17 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,22 @@
+package gestaoversoes;
+
+public class C {
+    private String C1;
+    private int C2;
+    
+    public String getC1(){
+        return this.C1;
+    }
+    
+    public int getC2(){
+        return this.C2;
+    }
+    
+    public void MC1(){
+        System.out.println("MC1()");
+    }
+    
+    public void MC2(){
+        System.out.println("MC2()");
+    }
+}
