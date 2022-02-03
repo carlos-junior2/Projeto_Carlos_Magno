0f2c097b3275299fca4c57de47f0639c64280c96
carlos-junior2 <69009883+carlos-junior2@users.noreply.github.com>
2022-02-03 18:00:46 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,22 @@
+package gestaoversoes;
+
+public class A {
+    private int A1;
+    private float A2;
+       
+    public int getA1(){
+        return this.A1;
+    }
+    
+    public float getA2(){
+        return this.A2;
+    }
+    
+    public void MA1(){
+        System.out.println("MA1()");
+    }
+    
+    public void MA2(){
+        System.out.println("MA2()");
+    }
+}
---
+++ B.java
@@ -0,0 +1,22 @@
+package gestaoversoes;
+
+public class B {
+    private int B1;
+    private float B2;
+    
+    public int getB1(){
+        return this.B1;
+    }
+    
+    public float getB2(){
+        return this.B2;
+    }
+    
+    public void MB1(){
+        System.out.println("MB1()");
+    }
+    
+    public void MB2(){
+        System.out.println("MB2()");
+    }
+}
