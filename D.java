62c98f0713f836d3e4111dc798e791f757f85879
unknown <juninhonasc3@gmail.com>
2022-02-03 22:05:13 -0300

    Criação da classe D

---
+++ D.java
@@ -0,0 +1,22 @@
+package gestaoversoes;
+
+public class D {
+    private String D1;
+    private int D2;
+    
+    public String getD1(){
+        return this.D1;
+    }
+    
+    public int getD2(){
+        return this.D2;
+    }
+    
+    public void MD1(){
+        System.out.println("MD1()");
+    }
+    
+    public void MD2(){
+        System.out.println("MD2()");
+    }
+}
